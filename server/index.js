892e982db7a88ec01a29b4e9ab64557e5ddce2c8
暮雪初心 <1329756466@qq.com>
2018-10-04 23:30:45 +0800

    新增静态文件

--- server/index.js
+++ server/index.js
@@ -1,4 +1,13 @@
 const Koa = require('koa')
 const app = new Koa()
+const { normal } = require('./tpl')
 
 app.listen(5000)
+
+app.use(async (ctx, next) => {
+    ctx.type = 'text/html; charset=utf-8'
+    ctx.body = normal
+})
+
+app.listen(5001
+)
